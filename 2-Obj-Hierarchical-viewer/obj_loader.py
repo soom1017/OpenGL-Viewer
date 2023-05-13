1168f42ab204d96b165ea829add8785b4481a258
soom1017 <min001017@naver.com>
2023-05-13 16:40:27 +0900

    feat: print out obj info

--- 2-Obj-Hierarchical-viewer/obj_loader.py
+++ 2-Obj-Hierarchical-viewer/obj_loader.py
@@ -1,8 +1,12 @@
 import numpy as np
 import glm
+import os
 
 class Material:
     def __init__(self, filename):
+        # print out obj file name
+        print(f'obj file name: {os.path.basename(filename)}')
+        # get vertex, face information from obj file
         self.v, self.f, self.vn = self.load_obj(filename)
         self.index_count = len(self.f[0])
         
@@ -10,6 +14,10 @@ class Material:
         vertexes = []
         faces = [[], [], []]
         normals = []
+
+        num_face_tri = 0
+        num_face_quad = 0
+        num_face_n = 0
         try:
             with open(filename) as file:
                 for line in file:
@@ -20,19 +28,32 @@ class Material:
                         
                     elif line.startswith("f "):
                         f = line.split()[1:]
-                        for idx in range(1, len(f) - 1):
+                        num_vertex = len(f)
+                        for idx in range(1, num_vertex - 1):
                             for i in [0, idx, idx+1]:
                                 face = f[i].split('/')
                                 for j in range(len(face)):
                                     if face[j] != '':
                                         faces[j].append(int(face[j]) - 1)
-                        
+                        if num_vertex == 3:
+                            num_face_tri += 1
+                        elif num_vertex == 4:
+                            num_face_quad += 1
+                        else:
+                            num_face_n += 1
+
                     elif line.startswith("vn "):
                         vn = line.split()[1:]
                         normals.append(list(map(float, vn)))
         except IOError:
             print(f"Error: Could not open file {file}")
             return None, None, None
+        # print out face informations
+        print(f'total number of faces: {num_face_tri + num_face_quad + num_face_n}')
+        print(f'number of faces with 3 vertices: {num_face_tri}')
+        print(f'number of faces with 4 vertices: {num_face_quad}')
+        print(f'number of faces with more than 4 vertices: {num_face_n}')
+        print('----------------------------------------------------------')
         return vertexes, faces, normals
     
     def get_vertex_pos(self):
